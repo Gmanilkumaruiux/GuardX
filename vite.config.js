be75e011730ce22f1d164f70708eeef0adfa6f1e
Manil Galla <manilgalla039@email.com>
2026-05-23 11:44:40 +0530

    fix: make asset base path conditional for local serve and production build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/GuardX/"
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '/' : '/GuardX/',
+    plugins: [
+      tailwindcss(),
+      react()
+    ],
+  }
 })
